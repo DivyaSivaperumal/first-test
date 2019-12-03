5c18382a478ceefc20d015d921b4927f953c93f9
vimalaadhithanl <vimalaadhithanl@CFLVIMALAADHIT.Virtusa.com>
2019-12-03 16:52:10 +0530

    commit1

--- BeforeAll/BaseTest.js
+++ BeforeAll/BaseTest.js
@@ -1,7 +1,7 @@
 beforeAll(function()
 			{
 		browser.get("https://posse.com/")
-		// browser.get("https://angularjs.org/");
+	 // browser.get("https://angularjs.org/");
 			});
 	afterAll(function()
 			   {
